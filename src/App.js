1482bb1afc791fea8ce46eff0fce0e2b2d41b5cf
samia-cherni <samiacherni@outlook.com>
2021-05-08 15:59:04 +0100

    adding changes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import img from './imageInSrc.jpg'
 function App() {
   return (
     <div className="App">
-      <div style={{border:'solid 1px black', maxWidth:'100vw'}}>
-      <h1 className="title red">Your name here</h1>
-      <br />
-      <img src={img} alt=" in source"/>
-      <br />
-      <img src="/imageInPublic.jpg" alt=" in public"/>
+        <div className="container"style={{border:'solid 1px black', maxWidth:'100vw'}}>
+        <div className="row">
+          <div className="col"></div>
+          <div className="col-6">
+            <h1 className="title red">Animal pictures and videos</h1>
+            <br />
+            <img className="rounded img-fluid" src={img} alt="in source"/>
+            <br />
+            <img className="rounded img-fluid" src="/imageInPublic.jpg" alt="in public"/>
+          </div>
+          <div className="col"></div>
+          </div>
+        </div>
+              <video width={320} height={240} controls>
+                <source src="/myVideo.mp4" type="video/mp4" />
+              </video>
       </div>
-      <video width={320} height={240} controls>
-        <source src="myVideo.mp4" type="video/mp4" />
-      </video>
-    </div>
   );
 }
 
